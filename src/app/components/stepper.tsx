40f8751e9bb6f4693d8b2410cb1785ffa260169b
Tomas Gonzalez <tomimateo2008@hotmail.com>
2024-11-16 02:53:29 -0300

    Actualizacion

--- src/app/components/stepper.tsx
+++ src/app/components/stepper.tsx
@@ -8,36 +8,46 @@ export default function Stepper(props: Props) {
       <div className="w-3/4 min-h-6">
         <ul className="flex flex-row overflow-hidden justify-between gap-6 rounded-2xl text-center ">
           <li
-            className={`bg-white w-full ${
-              props.currentQuestions >= 1 ? "bg-redPrimary" : "text-black"
+            className={` w-full ${
+              props.currentQuestions >= 1
+                ? "bg-redPrimary text-white"
+                : "bg-white text-black"
             }`}
           >
             1
           </li>
           <li
-            className={`bg-white w-full ${
-              props.currentQuestions >= 2 ? "bg-redPrimary" : "text-black"
+            className={`w-full ${
+              props.currentQuestions >= 2
+                ? "bg-redPrimary text-white"
+                : "bg-white text-black"
             }`}
           >
             2
           </li>
           <li
-            className={`bg-white w-full ${
-              props.currentQuestions >= 3 ? "bg-redPrimary" : " text-black"
+            className={` w-full ${
+              props.currentQuestions >= 3
+                ? "bg-redPrimary text-white"
+                : "bg-white text-black"
             }`}
           >
             3
           </li>
           <li
-            className={`bg-white w-full ${
-              props.currentQuestions >= 4 ? "bg-redPrimary" : "text-black"
+            className={` w-full ${
+              props.currentQuestions >= 4
+                ? "bg-redPrimary text-white"
+                : "bg-white text-black"
             }`}
           >
             4
           </li>
           <li
-            className={`bg-white w-full ${
-              props.currentQuestions >= 5 ? "bg-redPrimary" : "text-black"
+            className={` w-full ${
+              props.currentQuestions >= 5
+                ? "bg-redPrimary text-white"
+                : "bg-white text-black"
             }`}
           >
             5
